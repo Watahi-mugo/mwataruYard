6a8afd270a08f211bdca27869086eef2077b4407
Sir_Mugo <mugopeter128@gmail.com>
2025-06-09 10:44:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,18 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <!-- owner dashboard -->
+    <nav class="">
+        <div class="">
+            <ul class="">
+                <li>Home</li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        
+    </nav>
+    
  
 <body>
 <script src="main.js"></script>
